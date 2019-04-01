e417ea1316608cb1a37e19e9f1acd6ba02685ddf
Lory Jiang <funtree9@icloud.com>
2019-04-01 14:28:33 +0800

    some improvemenbt

--- mailController.js
+++ mailController.js
@@ -1,19 +1,19 @@
 const QQMailService = require('./qqMailService')
 
 exports.login = async (req, res) => {
-	try {
-		await QQMailService.initQQMail(req.body)
-		return res.json({success: true}).status(200)
-	} catch (error) {
-		return res.status(203).json({success: false, errorMsg: error.message})
-	}
+  try {
+    await QQMailService.initQQMail(req.body)
+    return res.json({success: true}).status(200)
+  } catch (error) {
+    return res.status(203).json({success: false, errorMsg: error.message})
+  }
 }
 
 exports.send = async (req, res) => {
-	try {
-		await QQMailService.newQQMail(req.body)
-		return res.json({success: true}).status(200)
-	} catch (error) {
-		return res.status(203).json({success: false, errorMsg: error.message})
-	}
+  try {
+    await QQMailService.newQQMail(req.body)
+    return res.json({success: true}).status(200)
+  } catch (error) {
+    return res.status(203).json({success: false, errorMsg: error.message})
+  }
 }
--- qqMailService.js
+++ qqMailService.js
@@ -1,15 +1,11 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
 
-let data = require('fs').readFileSync('./data.csv').toString().split('\n').map(_ => _.split(','))
-let fieldName = data.shift()
-let dataArray = data.map(_ => fieldName.reduce((a, b, index) => Object.assign(a, {[b]:_[index]}), {}))
-
-let user2Mailer = new Map()
+const user2Mailer = new Map()
 
 exports.initQQMail = async ({user = '948471414', pass = 'wyrxdbhgqrcjbdah'}) => {
   user2Mailer.delete(user)
-  let transporter = nodemailer.createTransport({
-    host: "smtp.qq.com",
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.qq.com',
     port: 465,
     secure: true, // secure:true for port 465, secure:false for port 587
     pool: true,
@@ -19,7 +15,7 @@ exports.initQQMail = async ({user = '948471414', pass = 'wyrxdbhgqrcjbdah'}) => 
     },
     tls: {
       rejectUnauthorized: false
-    },
+    }
   })
   await transporter.verify()
   transporter.pass = pass
@@ -27,12 +23,13 @@ exports.initQQMail = async ({user = '948471414', pass = 'wyrxdbhgqrcjbdah'}) => 
   return transporter
 }
 
-exports.newQQMail = async ({user = '948471414', pass = 'wyrxdbhgqrcjbdah', subject, from = "LoryJiang <948471414@qq.com>", to = 'funtree9@icloud.com', text, html = "<h1>whoareyou</h1>", cc, bcc}) => {
+exports.newQQMail = async ({user = '948471414', pass = 'wyrxdbhgqrcjbdah', subject, from = 'LoryJiang <948471414@qq.com>', to = 'funtree9@icloud.com', text, html = '<h1>whoareyou</h1>', cc, bcc}) => {
   let mailer = user2Mailer.get(user)
   console.log(mailer)
-    !mailer &&  (mailer = await exports.initQQMail({user, pass}))
-  let mailOptions = {    from,    to,    subject,    text,    html  }
+  !mailer && (mailer = await exports.initQQMail({user, pass}))
+  const mailOptions = {from, to, subject, text, html}
   await mailer.sendMail(mailOptions)
+  console.log(to + 'send success')
 }
 
 // create reusable transporter object using the default SMTP transport
